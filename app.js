b8ad2360a9259dd5f9bb75400e0ea3960baebe24
unknown <roshananand2005@gmail.com>
2024-06-01 22:19:16 +0530

    deploy test 3

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ mongoose.connect(process.env.MONGO_URI)
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 // view engine setup
